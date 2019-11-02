739ab97e917e4326d57a71a2411af19a23b3af22
Johannes Wuensch <j.wuensch@protonmail.com>
2019-11-02 13:39:41 +0100

    refactor hovercraft.js + assign classes and body class also after init

--- hovercraft/templates/pro-template/js/hovercraft.js
+++ hovercraft/templates/pro-template/js/hovercraft.js
@@ -37,23 +37,25 @@
     }
 
 
-
-    // assign a class to each step that defines the desired background for that step.
-    // This replicates the last defined background info for each step until there is a step that already defines it. Then this info will be replicated further down the line...
+    // This happens after initialization. We now apply some classes for each step and update the background class for the body, so it also works on the first load, not just when we made the first transition.
     var rootId = rootId || "impress";
     var root = util.byId( rootId );
     var steps = util.$$( ".step", root );
+    var current_step_number = get_current_step();
     var current_bg_img_class = ""
+    
+    console.log(current_step_number);
 
     steps.forEach(function(step) {
 
         var number_of_classes = step.classList.length;
         var step_already_has_background_class = false;
 
+        // replicate background class from step before if this step has no class defining the background.
         for (var i = 0; i < number_of_classes; i++) {
             var class_name = step.classList.item(i);
 
-            if (class_name.includes("bg_img_")){
+            if (class_name.includes("body--")){
                 step_already_has_background_class = true;
                 current_bg_img_class = class_name;
             }
@@ -64,12 +66,34 @@
                 step.classList.add( current_bg_img_class );
             }  
         }
+
+
+        // apply "over" and "coming" classes
+        var loop_step_number = parseInt(step.attributes["step"].value);
+
+        if (loop_step_number + 1 < current_step_number){
+            step.classList.add( "over" );
+            step.classList.remove( "coming" );
+        }
+        else if (loop_step_number + 1 > current_step_number){
+            step.classList.add( "coming" );
+            step.classList.remove( "over" );
+        }
+
+        else {
+            step.classList.remove( "coming" );
+            step.classList.remove( "over" );
+
+            update_body_classList_with_background_info(step);
+        }
+
+
      });
 
     
 
 
-    // apply "coming" and "over" class on stepleave + fade background if desired by applying a respective class to the body
+    // apply "coming" and "over" class on stepleave + apply specific class to body if defined in step
     document.getElementById("impress").addEventListener( "impress:stepleave", function (event) {
         
         console.log("leave event triggered!");
@@ -102,62 +126,8 @@
                 step.classList.remove( "coming" );
                 step.classList.remove( "over" );
 
-                
-                // We check this step for it's background information. Because we want to fade the background to the step we are appraching.
-                // If it has some background information we will take that class name which starts with "bg_img_" and add a class to the body that starts with "bg__" and goes on like the one on the step.
-                // So "bg_img_5" class on a step will translate to "bg__5" class on the body object. While deleting all other "bg__" classes
-                // This means that we can now apply classes like "bg_img_something" to a step without defining that class in css. So it will do nothing. Instead we define "bg__something" which defines the background image of the body. And voila, we have a fading background image.
-
-                var number_of_classes = step.classList.length;
-                var step_includes_bg_information = false;
-
-                for (var i = 0; i < number_of_classes; i++) {
-                    var class_name = step.classList.item(i);
-                    
-                    if (class_name.includes("bg_img_")){
-                        
-                        step_includes_bg_information = true;
-
-                        var translated_class_name =class_name.replace("img", "");
-                        
-                        
-
-                        var body = document.getElementsByTagName("BODY")[0];
-
-                        var num_of_body_classes = body.classList.length;
-
-                        for (var j = 0; j < num_of_body_classes; j++) {
-                            
-                            var body_class_name = body.classList.item(j);
-                            
-                            if (body_class_name != null && body_class_name.includes("bg__")){
-                                
-                                if (body_class_name != translated_class_name){
-                                    body.classList.remove(body_class_name)
-                                } 
-                            }
-                        }
-
-                        body.classList.add(translated_class_name);
-                    }
-                }
-                    
-                // the step has no class that represents a background (starts with "bg_img_")
-                if (!step_includes_bg_information){
-                    console.log("my current class does not have background information");
-                    var body = document.getElementsByTagName("BODY")[0];
-
-                    var num_of_body_classes = body.classList.length;
-
-                    for (var j = 0; j < num_of_body_classes; j++) {
-                        
-                        var body_class_name = body.classList.item(j);
-                        if (body_class_name != null && body_class_name.includes("bg__")){
-                                body.classList.remove(body_class_name)  
-                        }
-                    }
-                }
-                    
+                update_body_classList_with_background_info(step);
+                   
             }
             
         });
@@ -180,9 +150,97 @@
 }) (document);
 
 
+
+
 // Function updating the slide number counter
 function update_slide_number(evt)
 {
     var step = evt.target.attributes['step'].value;
     document.getElementById('slide-number').innerText = parseInt(step) + 1;
+}
+
+
+
+
+
+// This function will apply a specific class to he body tag if the step defines one.
+// The "body--" class on a step will translate to "body__" class on the body object. While deleting all other "body__" classes. So there is always only one "body__" class on body.
+// This means that we can now apply classes like "body--something" to a step without defining that class in css. So it will do nothing. 
+// Instead we define "body__something" which for example defines the background image of the body. And voila, we have a fading background image!
+function update_body_classList_with_background_info(step)
+{
+    var number_of_classes = step.classList.length;
+    var step_includes_bg_information = false;
+
+    for (var i = 0; i < number_of_classes; i++) {
+        var class_name = step.classList.item(i);
+        
+        // step has a class that defines a specific body class (starts with "body--")
+        if (class_name.includes("body--")){
+            
+            step_includes_bg_information = true;
+
+            var translated_class_name =class_name.replace("--", "__");
+            
+            
+
+            var body = document.getElementsByTagName("BODY")[0];
+
+            var num_of_body_classes = body.classList.length;
+
+            for (var j = 0; j < num_of_body_classes; j++) {
+                
+                var body_class_name = body.classList.item(j);
+                
+                if (body_class_name != null && body_class_name.includes("body__")){
+                    
+                    if (body_class_name != translated_class_name){
+                        body.classList.remove(body_class_name)
+                    } 
+                }
+            }
+
+            body.classList.add(translated_class_name);
+        }
+    }
+        
+    // step has no class that defines a specific body class (none starts with "body--")
+    if (!step_includes_bg_information){
+
+        var body = document.getElementsByTagName("BODY")[0];
+
+        var num_of_body_classes = body.classList.length;
+
+        for (var j = 0; j < num_of_body_classes; j++) {
+            
+            var body_class_name = body.classList.item(j);
+            if (body_class_name != null && body_class_name.includes("body__")){
+                    body.classList.remove(body_class_name)  
+            }
+        }
+    }
+        
+}
+
+
+
+
+// This function will return the current step as integer
+function get_current_step()
+{
+    var body = document.getElementsByTagName("BODY")[0];
+
+    var num_of_body_classes = body.classList.length;
+
+    for (var j = 0; j < num_of_body_classes; j++) {
+        
+        var body_class_name = body.classList.item(j);
+        
+        if (body_class_name.includes("impress-on-step-")){
+            
+            if (body_class_name != null && body_class_name.includes("impress-on-step-")){
+                return  parseInt( body_class_name.replace("impress-on-step-", "") );
+            } 
+        }
+    }   
 }
